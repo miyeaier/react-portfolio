1445a336bf5cb20d2be670770d53671c18b928ad
miyesier_kaerman <miyesier@gmail.com>
2019-11-18 11:58:41 +0100

    add image

--- src/About.jsx
+++ src/About.jsx
@@ -15,7 +15,13 @@ import React from "react"
   <p>My name is Miyesier Kaerman,Im from Kashgar,Now studyingat Craft Academy ,I am Barista who does not drink coffee and is</p>
   <img class="ui small right floated image" src="https://github.com/miyeaier/react-portfolio/blob/master/Img/pic.jpeg?raw=true"></img>
   <p>also a photographer, nature lovers</p>
+ <br/>
    <p>Because I like to learn different knowledge and I wish to working IT  industry I started learning coding at Craft Academy</p>
+   <br/>
+   <p>Svart trumpetsvamp (Craterellus cornucopioides) är en helt svart, ibland närmast mörkblå svamp i kantarellsläktet. Den är en populär matsvamp särskilt lämplig att torka. Den sägs växa på marker där ek och hassel förekommer samt i barrskog. Svampen är trattformad och har ett tunt kött. Den kan växa väl dold under vissnande löv och inne i mossa. Ibland återfinns den som "deformerade trattar", intrasslade i gräs. Så länge inte fruktkropparna är ruttna försämrar det dock inte matvärdet på något vis.
+</p>
+<br/>
+<p>Trattkantarell (Craterellus tubaeformis) är en vanlig matsvamp [1][2] i Europa, Sibirien och Nordamerika. Trattkantarellen är ganska vanlig i södra och mellersta Sverige men mer sällsynt i norra delen av landet samt i sura områden. Trattkantarellen kan bli så stor som 12 cm på höjden och 5 cm på bredden. Den hittas i september till november.</p>
 </div>
 
 
